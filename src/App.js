309d824add6c03e931888d4a44d21faa4a0f206c
patrick-rush <patrick.a.rush@gmail.com>
2021-03-13 22:03:32 -0500

    create navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      HELLO
+      <Router>
+        <nav className="text-center bg-blue-900 text-yellow-100 p-4">
+          <NavLink 
+            exact
+            to="/"
+            className="inline-block px-4 py-2"
+            activeClassName="text-red-300"
+            >Plants
+          </NavLink>
+          <NavLink
+            exact
+            to="/plants"
+            className="inline-block px-4 py-2"
+            activeClassName="text-red-300"
+            >Today
+          </NavLink>
+          <NavLink
+            exact
+            to="/plants/new"
+            className="inline-block px-4 py-2"
+            activeClassName="text-red-300"
+            >New Plant
+          </NavLink>
+        </nav>
+        <Switch>
+          <Route exact path="/" >route 1</Route>
+          <Route exact path="/plants" >route 2</Route>
+          <Route exact path="/plants/:plantId" >route 3</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
