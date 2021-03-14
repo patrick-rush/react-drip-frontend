97b134f4f17b9c906b8f0ab0edd36a621ed1faec
patrick-rush <patrick.a.rush@gmail.com>
2021-03-14 14:35:09 -0400

    move nav functionality

--- src/App.js
+++ src/App.js
@@ -3,50 +3,18 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  NavLink
+  // NavLink
 } from 'react-router-dom';
 import PageContainer from './containers/PageContainer';
-import { connect } from 'react-redux'
-import { manageNavigation } from './actions/page'
+// import { connect } from 'react-redux'
+// import { manageNavigation } from './actions/page'
+import NavigationContainer from './containers/NavigationContainer'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <nav className="bg-green-800">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex items-center justify-between h-16">
-              <div className="flex items-center">
-                <div className="">
-                  <div className="ml-10 flex items-baseline space-x-4">
-                    <NavLink 
-                      exact
-                      to="/"
-                      // onClick={this.manageNavigation("welcome")}
-                      className="hover:bg-green-700 hover:text-white bg-green-800 text-white px-3 py-2 rounded-md text-sm font-medium"
-                      activeClassName="text-green-100 bg-green-900"
-                      >Plants
-                    </NavLink>
-                    <NavLink
-                      exact
-                      to="/plants"
-                      className="hover:bg-green-700 hover:text-white bg-green-800 text-white px-3 py-2 rounded-md text-sm font-medium"
-                      activeClassName="text-green-100 bg-green-900"
-                      >Today
-                    </NavLink>
-                    <NavLink
-                      exact
-                      to="/plants/new"
-                      className="hover:bg-green-700 hover:text-white bg-green-800 text-white px-3 py-2 rounded-md text-sm font-medium"
-                      activeClassName="text-green-100 bg-green-900"
-                      >New Plant
-                    </NavLink>
-                  </div>
-                </div>  
-              </div>
-            </div>
-          </div>  
-        </nav>
+        <NavigationContainer />
         <Switch>
           <Route exact path="/" component={PageContainer}></Route>
           <Route exact path="/plants" >route 2</Route>
@@ -58,14 +26,4 @@ function App() {
   );
 }
 
-const mapStateToProps = state => {
-
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    manageNavigation: (location) => dispatch(manageNavigation(location))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
