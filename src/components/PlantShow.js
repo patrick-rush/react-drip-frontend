86bf29b843bc0ef6c9abcc4ac97ce95a635c1960
patrick-rush <patrick.a.rush@gmail.com>
2021-03-14 19:07:55 -0400

    change 'class' to 'className'

--- src/components/PlantShow.js
+++ src/components/PlantShow.js
@@ -4,38 +4,38 @@ class PlantShow extends Component {
     render() {
         return (
             <ul>
-                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <li class="text-sm font-medium text-gray-500">
+                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <li className="text-sm font-medium text-gray-500">
                         Location
                     </li>
-                    <li class="location mt-1 text-sm text-green-900 sm:mt-0 sm:col-span-1">
+                    <li className="location mt-1 text-sm text-green-900 sm:mt-0 sm:col-span-1">
                         {this.props.plant.location}
                     </li>
-                    <li class="mt-1 text-sm text-green-900 sm:mt-0 sm:col-span-1">
-                        <a href="#edit" class="my-4 pr-2 text-right"><i class="editPlant fa fa-pencil-alt content-end"></i></a>      
-                        <a href="#delete" class="my-4 p-2 text-right"><i class="deletePlant fa fa-trash content-end"></i></a>      
+                    <li className="mt-1 text-sm text-green-900 sm:mt-0 sm:col-span-1">
+                        <a href="#edit" className="my-4 pr-2 text-right"><i className="editPlant fa fa-pencil-alt content-end"></i></a>      
+                        <a href="#delete" className="my-4 p-2 text-right"><i className="deletePlant fa fa-trash content-end"></i></a>      
                     </li>
                     </div>
-                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <li class="text-sm font-medium text-gray-500">
+                    <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <li className="text-sm font-medium text-gray-500">
                         Watering Frequency
                     </li>
-                    <li class="watering_frequency mt-1 text-sm text-green-900 sm:mt-0 sm:col-span-1">
+                    <li className="watering_frequency mt-1 text-sm text-green-900 sm:mt-0 sm:col-span-1">
                         {this.props.plant.watering_frequency} days
                     </li>
-                    <li class="mt-1 text-sm text-green-900 sm:mt-0 sm:col-span-1">
-                        <a href="#decrease" class="my-4 pr-2 text-right"><i class="fa fa-minus decreaseDays"></i></a>
-                        <a href="#increase" class="my-4 p-2 text-right"><i class="fa fa-plus increaseDays"></i></a>      
+                    <li className="mt-1 text-sm text-green-900 sm:mt-0 sm:col-span-1">
+                        <a href="#decrease" className="my-4 pr-2 text-right"><i className="fa fa-minus decreaseDays"></i></a>
+                        <a href="#increase" className="my-4 p-2 text-right"><i className="fa fa-plus increaseDays"></i></a>      
                     </li>
                     </div>
-                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:px-6">
-                    <li class="notesContainer text-sm font-medium text-gray-500">
-                        Notes <a href="#addNote" class="my-4 p-2 text-right"><i class="fa fa-plus addNoteIcon"></i></a>
+                    <div className="bg-gray-50 px-4 py-5 sm:grid sm:px-6">
+                    <li className="notesContainer text-sm font-medium text-gray-500">
+                        Notes <a href="#addNote" className="my-4 p-2 text-right"><i className="fa fa-plus addNoteIcon"></i></a>
                     </li>
                     </div>  
-                    <div class="bg-white px-4 py-5 sm:grid sm:px-6">
-                    <li class="careEventsContainer text-sm font-medium text-gray-500">
-                        Care Events <a href="#addCareEvent" class="my-4 p-2 text-right"><i class="fa fa-plus addCareEventIcon"></i></a>
+                    <div className="bg-white px-4 py-5 sm:grid sm:px-6">
+                    <li className="careEventsContainer text-sm font-medium text-gray-500">
+                        Care Events <a href="#addCareEvent" className="my-4 p-2 text-right"><i className="fa fa-plus addCareEventIcon"></i></a>
                     </li>
                 </div>  
             </ul>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,9 +4,9 @@ class Welcome extends Component {
     render() {
         return (
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-                <h2 class="text-3xl font-extrabold tracking-tight text-green-900 sm:text-4xl">
-                    <span class="block">Welcome to Drip</span>
-                    <span class="block text-green-600">Keep those houseplants lookin their best</span>
+                <h2 className="text-3xl font-extrabold tracking-tight text-green-900 sm:text-4xl">
+                    <span className="block">Welcome to Drip</span>
+                    <span className="block text-green-600">Keep those houseplants lookin their best</span>
                 </h2>
             </div>
         )
