a9855829ff5ce981c55ecca277fab1d49dde8e68
patrick-rush <patrick.a.rush@gmail.com>
2021-03-14 11:46:42 -0400

    change to left and right containers

--- src/containers/LeftContainer.js
+++ src/containers/LeftContainer.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import Plant from '../components/Plant';
 import { connect } from 'react-redux';
+import Header from '../components/Header';
 
-class PlantIndexContainer extends Component {
+class LeftContainer extends Component {
     
     renderPlants = () => this.props.plants.map(plant => <Plant plant={plant} history={this.props.history}/>)
     
     render() {
         return (
             <div className="bg-white shadow overflow-hidden sm:rounded-lg">
-                <div className="px-4 py-5 sm:px-6">
+                {/* <div className="px-4 py-5 sm:px-6">
                     <h3 className="text-lg leading-6 font-medium text-green-900">
                         Plants
                     </h3>
-                </div>
+                </div> */}
+                <Header />
                 <div className="border-t border-gray-200">
                 </div>
                 <div>
@@ -37,4 +39,4 @@ const mapStateToProps = state => {
 //     }
 // }
 
-export default connect(mapStateToProps)(PlantIndexContainer);
+export default connect(mapStateToProps)(LeftContainer);
--- src/containers/RightContainer.js
+++ src/containers/RightContainer.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
+import Welcome from '../components/Welcome';
 
-class PlantShowContainer extends Component {
+class RightContainer extends Component {
     render() {
         return (
             <div className="overflow-hidden bg-white sm:min-h-screen col-span-2 rounded-md shadow">
-                <div className="overflow-hidden px-4 py-5 sm:px-6">
+                {/* <div className="overflow-hidden px-4 py-5 sm:px-6">
                     <h3 className="overflow-hidden text-lg leading-6 font-medium text-green-900">
                     <br />
                     </h3>
-                </div>
+                </div> */}
+                <Header />
                 <div className="border-t border-gray-200">
-                    <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+                    {/* <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
                         <h2 class="text-3xl font-extrabold tracking-tight text-green-900 sm:text-4xl">
                             <span class="block">Welcome to Drip</span>
                             <span class="block text-green-600">Keep those houseplants lookin their best</span>
                         </h2>
-                    </div>
+                    </div> */}
+                    <Welcome />
                 </div>
             </div>
         )
     }
 }
 
-export default PlantShowContainer;
+export default RightContainer;
