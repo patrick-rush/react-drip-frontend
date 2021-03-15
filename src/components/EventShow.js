f13b583caeb8a46ff1f861b50507088f1889c48e
patrick-rush <patrick.a.rush@gmail.com>
2021-03-15 11:05:48 -0400

    convert a tags to buttons

--- src/components/EventShow.js
+++ src/components/EventShow.js
@@ -12,8 +12,8 @@ class EventShow extends Component {
                     Hello World
                 </li>
                 <li className="mt-1 text-sm text-green-900 sm:mt-0 sm:col-span-1">
-                    <a href="#decrease" className="my-4 pr-2 text-right"><i className="fa fa-minus decreaseDays"></i></a>
-                    <a href="#increase" className="my-4 p-2 text-right"><i className="fa fa-plus increaseDays"></i></a>      
+                    <button className="pl-2 pr-2 text-sm font-medium text-gray-500"><i className="fa fa-minus decreaseDays"></i></button>
+                    <button className="pl-2 pr-2 text-sm font-medium text-gray-500"><i className="fa fa-plus increaseDays"></i></button>      
                 </li>
                 </div>
                 <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -24,8 +24,8 @@ class EventShow extends Component {
                     Hello World
                 </li>
                 <li className="mt-1 text-sm text-green-900 sm:mt-0 sm:col-span-1">
-                    <a href="#complete" className="my-4 pr-2 text-right"><i className="fas fa-check completed"></i></a>      
-                    <a href="#delete" className="my-4 p-2 text-right"><i className="deleteCareEvent fa fa-trash content-end"></i></a>      
+                    <button className="pl-2 pr-2 text-sm font-medium text-gray-500"><i className="fas fa-check completed"></i></button>      
+                    <button className="pl-2 pr-2 text-sm font-medium text-gray-500"><i className="deleteCareEvent fa fa-trash content-end"></i></button>      
                 </li>
                 </div>
             </ul>
--- src/components/PlantShow.js
+++ src/components/PlantShow.js
@@ -12,30 +12,30 @@ class PlantShow extends Component {
                         {this.props.plant.location}
                     </li>
                     <li className="mt-1 text-sm text-green-900 sm:mt-0 sm:col-span-1">
-                        <a href="#edit" className="my-4 pr-2 text-right"><i className="editPlant fa fa-pencil-alt content-end"></i></a>      
-                        <a href="#delete" className="my-4 p-2 text-right"><i className="deletePlant fa fa-trash content-end"></i></a>      
+                        <button className="pl-2 pr-2 text-sm font-medium text-gray-500"><i className="editPlant fa fa-pencil-alt content-end"></i></button>      
+                        <button className="pl-2 pr-2 text-sm font-medium text-gray-500"><i className="deletePlant fa fa-trash content-end"></i></button>      
                     </li>
-                    </div>
-                    <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                </div>
+                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                     <li className="text-sm font-medium text-gray-500">
                         Watering Frequency
                     </li>
                     <li className="watering_frequency mt-1 text-sm text-green-900 sm:mt-0 sm:col-span-1">
                         {this.props.plant.watering_frequency} days
                     </li>
-                    <li className="mt-1 text-sm text-green-900 sm:mt-0 sm:col-span-1">
-                        <a href="#decrease" className="my-4 pr-2 text-right"><i className="fa fa-minus decreaseDays"></i></a>
-                        <a href="#increase" className="my-4 p-2 text-right"><i className="fa fa-plus increaseDays"></i></a>      
+                    <li className="mt-1 text-sm col-span-2 text-green-900 sm:mt-0 sm:col-span-1">
+                        <button className="pl-2 pr-2 text-sm font-medium text-gray-500"><i className="fa fa-minus decreaseDays"></i></button>
+                        <button className="pl-2 pr-2 text-sm font-medium text-gray-500"><i className="fa fa-plus increaseDays"></i></button>      
                     </li>
-                    </div>
-                    <div className="bg-gray-50 px-4 py-5 sm:grid sm:px-6">
+                </div>
+                <div className="bg-gray-50 px-4 py-5 sm:grid sm:px-6">
                     <li className="notesContainer text-sm font-medium text-gray-500">
-                        Notes <a href="#addNote" className="my-4 p-2 text-right"><i className="fa fa-plus addNoteIcon"></i></a>
+                        Notes <button className="pl-2 pr-2 text-sm font-medium text-gray-500"><i className="fa fa-plus addNoteIcon"></i></button>
                     </li>
-                    </div>  
-                    <div className="bg-white px-4 py-5 sm:grid sm:px-6">
+                </div>  
+                <div className="bg-white px-4 py-5 sm:grid sm:px-6">
                     <li className="careEventsContainer text-sm font-medium text-gray-500">
-                        Care Events <a href="#addCareEvent" className="my-4 p-2 text-right"><i className="fa fa-plus addCareEventIcon"></i></a>
+                        Care Events <button className="pl-2 pr-2 text-sm font-medium text-gray-500"><i className="fa fa-plus addCareEventIcon"></i></button>
                     </li>
                 </div>  
             </ul>
