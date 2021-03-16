732c1a3c941f7af172a8ed170af94c1bb87cbb84
patrick-rush <patrick.a.rush@gmail.com>
2021-03-15 22:19:15 -0400

    reformat

--- src/components/PlantShow.js
+++ src/components/PlantShow.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 class PlantShow extends Component {
+    
     render() {
         return (
             <ul>
@@ -12,8 +13,12 @@ class PlantShow extends Component {
                         {this.props.plant.location}
                     </li>
                     <li className="mt-1 text-sm text-green-900 sm:mt-0 sm:col-span-1">
-                        <button className="pl-2 pr-2 text-sm font-medium text-gray-900"><i className="editPlant fa fa-pencil-alt content-end"></i></button>      
-                        <button className="pl-2 pr-2 text-sm font-medium text-gray-900"><i className="deletePlant fa fa-trash content-end"></i></button>      
+                        <button onClick={this.handleEdit} className="pl-2 pr-2 text-sm font-medium text-gray-900">
+                            <i className="editPlant fa fa-pencil-alt content-end"></i>
+                        </button>      
+                        <button className="pl-2 pr-2 text-sm font-medium text-gray-900">
+                            <i className="deletePlant fa fa-trash content-end"></i>
+                        </button>      
                     </li>
                 </div>
                 <div className="opacity-80 m-2 bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -24,18 +29,28 @@ class PlantShow extends Component {
                         {this.props.plant.watering_frequency} days
                     </li>
                     <li className="mt-1 text-sm col-span-2 text-green-900 sm:mt-0 sm:col-span-1">
-                        <button className="pl-2 pr-2 text-sm font-medium text-gray-900"><i className="fa fa-minus decreaseDays"></i></button>
-                        <button className="pl-2 pr-2 text-sm font-medium text-gray-900"><i className="fa fa-plus increaseDays"></i></button>      
+                        <button className="pl-2 pr-2 text-sm font-medium text-gray-900">
+                            <i className="fa fa-minus decreaseDays"></i>
+                        </button>
+                        <button className="pl-2 pr-2 text-sm font-medium text-gray-900">
+                            <i className="fa fa-plus increaseDays"></i>
+                        </button>      
                     </li>
                 </div>
                 <div className="opacity-80 m-2 bg-gray-50 px-4 py-5 sm:grid sm:px-6">
                     <li className="notesContainer text-sm font-medium text-gray-900">
-                        Notes <button className="pl-2 pr-2 text-sm font-medium text-gray-900"><i className="fa fa-plus addNoteIcon"></i></button>
+                        Notes 
+                        <button className="pl-2 pr-2 text-sm font-medium text-gray-900">
+                            <i className="fa fa-plus addNoteIcon"></i>
+                        </button>
                     </li>
                 </div>  
                 <div className="opacity-80 m-2 bg-white px-4 py-5 sm:grid sm:px-6">
                     <li className="careEventsContainer text-sm font-medium text-gray-900">
-                        Care Events <button className="pl-2 pr-2 text-sm font-medium text-gray-900"><i className="fa fa-plus addCareEventIcon"></i></button>
+                        Care Events
+                        <button className="pl-2 pr-2 text-sm font-medium text-gray-900">
+                            <i className="fa fa-plus addCareEventIcon"></i>
+                        </button>
                     </li>
                 </div>  
             </ul>
