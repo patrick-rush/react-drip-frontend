072797e5239d71cf8dcdafa04244fce45918cafb
patrick-rush <patrick.a.rush@gmail.com>
2021-06-01 22:09:55 -0400

    fix event and plant buttons to remove focus ring

--- src/components/plants/Plant.js
+++ src/components/plants/Plant.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 function Plant({ plant, textColor, handleClick }) {
     return (
-        <div
-            className="bg-gray-50 m-2 px-4 py-5 sm:grid sm:grid-cols-1 sm:gap-4 sm:px-6">
-            <button onClick={() => handleClick(plant.id)} className={`text-left text-sm font-medium ${textColor}`}>
+        <div onClick={() => handleClick(plant.id)} className="cursor-pointer bg-gray-50 m-2 px-4 py-5 sm:grid sm:grid-cols-1 sm:gap-4 sm:px-6">
+            <span className={`cursor-pointer text-left text-sm font-medium ${textColor}`}>
                 {plant.name} the {plant.species}
-            </button>        
+            </span>        
         </div>
     )
 }
