4433c46ce58b244f0f92c0bf2fe798a5a9ec201a
patrick-rush <patrick.a.rush@gmail.com>
2021-06-02 19:25:03 -0400

    change event button to div

--- src/components/events/EventByPlant.js
+++ src/components/events/EventByPlant.js
@@ -24,10 +24,10 @@ class EventByPlant extends Component {
     
     render() {
         return (
-            <div className={"bg-gray-light shadow overflow-hidden sm:rounded-lg mt-5"}>
-                <button onClick={this.handleOnClick} disabled={this.props.event.completed} className={`text-sm font-medium px-4 py-5 col-span-full ${this.props.event.completed ? "line-through cursor-not-allowed" : null}`} >
+            <div onClick={this.handleOnClick} className={"bg-gray-light shadow overflow-hidden sm:rounded-lg mt-5 cursor-pointer"}>
+                <div disabled={this.props.event.completed} className={`text-sm font-medium px-4 py-5 col-span-full ${this.props.event.completed ? "line-through cursor-not-allowed" : null}`} >
                     {`${this.props.event.event_type} on ${this.formattedDueDate(this.props.event)}`}
-                </button>
+                </div>
             </div>
         )
     }
